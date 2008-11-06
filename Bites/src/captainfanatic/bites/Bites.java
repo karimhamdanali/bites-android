97effa7e90b20638602cc13a3ed747cd3e74be0f
benny.caldwell <benny.caldwell@fab3fe4f-4d48-0410-8693-7973330e2e89>
2008-11-06 11:45:13 +0000

    Added a view flipper example interface.

--- Bites/src/captainfanatic/bites/Bites.java
+++ Bites/src/captainfanatic/bites/Bites.java
@@ -2,12 +2,27 @@ package captainfanatic.bites;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class Bites extends Activity {
+	ViewFlipper flipper;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        flipper=(ViewFlipper)findViewById(R.id.details);
+        
+        Button btn = (Button)findViewById(R.id.flip_me);
+        
+        btn.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		flipper.showNext();
+        	}
+        });
     }
 }
