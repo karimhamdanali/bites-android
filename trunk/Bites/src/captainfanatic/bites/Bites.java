aaeb9de0c4b593997e7cb748a9a0a74978230322
benny.caldwell <benny.caldwell@fab3fe4f-4d48-0410-8693-7973330e2e89>
2008-11-16 13:25:20 +0000

    receives an sms and obtains the message body.

--- trunk/Bites/src/captainfanatic/bites/Bites.java
+++ trunk/Bites/src/captainfanatic/bites/Bites.java
@@ -14,6 +14,7 @@ import android.widget.ViewFlipper;
 public class Bites extends Activity {
 	ViewFlipper flipper;
 	TextView text;
+	SmsReceiver sms;
 	
     /** Called when the activity is first created. */
     @Override
@@ -58,5 +59,8 @@ public class Bites extends Activity {
         		flipper.showNext();
         	}
         });
+        
+        //Create an SmsReceiver broadcast listener
+        sms = new SmsReceiver();
     }
 }
