97d834c6717e9db603aa654ea9978338137ec385
benny.caldwell <benny.caldwell@fab3fe4f-4d48-0410-8693-7973330e2e89>
2008-11-25 13:07:38 +0000

    Recipe, Ingredients and Method array adapters implemented. Listviews added to viewflipper. Click on recipe goes to ingredients listview.

--- Bites/src/captainfanatic/bites/Bites.java
+++ Bites/src/captainfanatic/bites/Bites.java
@@ -6,10 +6,15 @@ import java.util.ArrayList;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -19,60 +24,60 @@ public class Bites extends Activity {
 	ViewFlipper flipper;
 	TextView text;
 	SmsReceiver sms;
-	Button btn; 
-	
+	Button btn;
+	ListView mRecipeListView;
+	ListView mRecipeIngredientsView;
+	ListView mRecipeMethodView;
+
 	ArrayList<Recipe> mRecipeList;
+	Recipe selRecipe;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-       
-        flipper=(ViewFlipper)findViewById(R.id.details);
-        text = (TextView)findViewById(R.id.TextView01);
+        
+        final Context context = this;
+        
         mRecipeList = new ArrayList<Recipe>();
-        String strXml = new String();
         Recipe recipe = new Recipe();
-        try {
-			strXml = recipe.Push();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		strXml = strXml.replaceAll("apples", "pears");
-		try {
-			recipe.Pull(strXml);
-		} catch (XmlPullParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		text.setText(strXml);
+        selRecipe = new Recipe();
+        mRecipeList.add(recipe);
+        mRecipeList.add(recipe);       
+        mRecipeListView = (ListView)findViewById(R.id.recipeListView);
+        mRecipeListView.setAdapter(new ArrayAdapter<Recipe>(this,
+                android.R.layout.simple_list_item_1,mRecipeList));
         
-        btn = (Button)findViewById(R.id.flip_me);
+        mRecipeIngredientsView = (ListView)findViewById(R.id.recipeIngredientsView);
+        mRecipeMethodView = (ListView)findViewById(R.id.recipeMethodView);
         
-        btn.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View view) {
-        		flipper.showNext();
-        	}
+		selRecipe = (Recipe)mRecipeListView.getItemAtPosition(0);
+        mRecipeIngredientsView.setAdapter(new ArrayAdapter<String>(context, 
+																	android.R.layout.simple_list_item_1, 
+																	selRecipe.ingredients));
+        mRecipeMethodView.setAdapter(new ArrayAdapter<String>(context, 
+        														android.R.layout.simple_list_item_1, 
+        														selRecipe.methods));
+        
+        flipper=(ViewFlipper)findViewById(R.id.details);
+        
+        mRecipeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> arg0, View arg1,
+											int arg2, long arg3) {
+				selRecipe = (Recipe)mRecipeListView.getSelectedItem();
+				ArrayAdapter<?> adapter = (ArrayAdapter<?>)mRecipeIngredientsView.getAdapter();
+				adapter.notifyDataSetChanged();
+				adapter = (ArrayAdapter<?>)mRecipeMethodView.getAdapter();
+				adapter.notifyDataSetChanged();
+				flipper.showNext();
+			}
         });
     }
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		setContentView(R.layout.main);
 		Intent intent = getIntent();
 		
 		//Check for new xml recipe from a text message etc.
