77486b53b2fcca6a8570041e7c069482811441eb
benny.caldwell <benny.caldwell@fab3fe4f-4d48-0410-8693-7973330e2e89>
2009-04-07 14:33:59 +0000

    Recipe receive works! Need to: Refresh selected recipe when all recipes are deleted. Possibly create branches for xml recipe format, and method with step number stored in an int column in sqlite for sorting.

--- branches/PlainTextSend/src/captainfanatic/bites/Bites.java
+++ branches/PlainTextSend/src/captainfanatic/bites/Bites.java
@@ -1,9 +1,15 @@
 package captainfanatic.bites;
 
+import captainfanatic.bites.RecipeBook.Ingredients;
+import captainfanatic.bites.RecipeBook.Methods;
+import captainfanatic.bites.RecipeBook.Recipes;
 import android.app.NotificationManager;
 import android.app.TabActivity;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TabHost;
 
@@ -20,12 +26,48 @@ public class Bites extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        if (getIntent().getAction() == "com.captainfanatic.bites.RECEIVED_RECIPE") {
-        	//Cancel the notification using the id extra in the intent
-    		NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-    		nm.cancel(getIntent().getIntExtra(SmsReceiver.KEY_NOTIFY_ID,0));
-    		//TODO: add recipe, ingredients and methods to content provider
-    		//TODO: select (highlight?) the new recipe in the RecipeList activity
+        /**
+         * If Bites was started by clicking on a notification that a recipe was received
+         * load the new recipe into the database and cancel the notification 
+         */
+        if (getIntent().getAction() != null)
+        {
+        	if(getIntent().getAction().contentEquals("com.captainfanatic.bites.RECEIVED_RECIPE")) 
+        	{
+	        	//Cancel the notification using the id extra in the intent
+	    		NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+	    		nm.cancel(getIntent().getIntExtra(SmsReceiver.KEY_NOTIFY_ID,0));
+	    		
+	    		ContentValues values = new ContentValues();
+	    		values.put(Recipes.TITLE, getIntent().getStringExtra(SmsReceiver.KEY_RECIPE));
+	    		Uri recipeUri = getContentResolver().insert(Recipes.CONTENT_URI, values);
+	    		long recipeId = Long.parseLong(recipeUri.getLastPathSegment());
+
+	    		//get ingredients from the intent extras and load into the content provider
+	    		String ingredients[] = getIntent().getStringArrayExtra(SmsReceiver.KEY_ING_ARRAY);
+	    			for (int i =0; i<ingredients.length; i++)
+		    		{
+		    			values = new ContentValues();
+		    			values.put(Ingredients.RECIPE, recipeId);
+		    			values.put(Ingredients.TEXT,ingredients[i]);
+		    			getContentResolver().insert(Ingredients.CONTENT_URI, values);
+		    		}
+	    		
+	    		//get methods from the intent extras and load into the content provider
+	    		String methods[] = getIntent().getStringArrayExtra(SmsReceiver.KEY_METH_ARRAY);
+	    		int methodSteps[] = getIntent().getIntArrayExtra(SmsReceiver.KEY_METH_STEP_ARRAY);
+	    		
+	    		for (int i = 0; i<methods.length; i++)
+	    		{
+	    			values = new ContentValues();
+	    			values.put(Methods.RECIPE, recipeId);
+	    			values.put(Methods.TEXT, methods[i]);
+	    			values.put(Methods.STEP, (i<methodSteps.length) ? methodSteps[i] : i);
+	    			getContentResolver().insert(Methods.CONTENT_URI, values);
+	    		}
+	    		
+	    		//TODO: select (highlight?) the new recipe in the RecipeList activity
+        	}
 		}
                
         final TabHost tabHost = getTabHost();
